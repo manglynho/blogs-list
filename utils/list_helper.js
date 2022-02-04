7e7f303a8530f45a402b68df8d2a511485c3970a
Reiner <manglynho@gmail.com>
2022-02-04 17:49:29 -0500

    excersices 4.3-4.7

---
+++ utils/list_helper.js
@@ -0,0 +1,43 @@
+require('lodash')
+const lodash = require('lodash')
+
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.map(el => el.likes).reduce((sum, val) => sum + val, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog)
+}
+
+const mostBlogs = (blogs) => {
+  let ranking = lodash.countBy(blogs, 'author')
+  let separated = lodash.map(ranking, function(el, index) {
+    return {
+      'author': index,
+      'blogs': el
+    }
+  })
+  return lodash.maxBy(separated, 'blogs')
+}
+
+const mostLikes= (blogs) => {
+  return lodash(blogs)
+    .groupBy('author')
+    .map((objs, key) => ({
+      'author': key,
+      'likes': lodash.sumBy(objs, 'likes')
+    }))
+    .value()
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+}
